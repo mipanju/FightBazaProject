4f81af3ffaf4cb0597d40cf58a5ca20b8204d822
Te0o <alexandru.teodor.at@gmail.com>
2024-01-25 21:33:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
     let SVG_female_frontElement = document.querySelector('.Female-Front');
     let SVG_female_backElement = document.querySelector ('.Female-Back');
     let buttonElement = document.querySelector('.js-swap_btn');
+    let btn = document.querySelector('#btn');
+    let sidebar = document.querySelector('.sidebar');
 function swap(){
     if (buttonElement.innerHTML === '<i class="bx bx-male-sign"></i>'){
     buttonElement.innerHTML = '<i class="bx bx-female-sign" ></i>';
@@ -20,10 +22,8 @@ function swap(){
     }
 
 
-    let btn = document.querySelector('#btn');
-    let sidebar = document.querySelector('.sidebar');
-    const screenWidth = window.innerWidth;
-    console.log(screenWidth);
+
+
      function resize(){
       sidebar.classList.toggle('active');
       if (sidebar.classList.contains('active' )){
@@ -45,7 +45,7 @@ function swap(){
       SVG_female_backElement.style.display = 'inline';
       buttonElement.style.display = 'inline';
     }
-    };
+    }
     function sidebar_toggle(){
       sidebar.classList.toggle('active');
     }
@@ -57,3 +57,14 @@ function swap(){
       }
      }
      btn.onclick = determine;
+     let instructions = document.querySelector('.exercises_display')
+     var computedStyle = window.getComputedStyle(instructions);
+     if (computedStyle.display === 'block'){
+      buttonElement.innerHTML = '<i class="bx bx-arrow-back"></i>'
+    }
+
+     function back(){
+     if  (buttonElement.innerHTML === '<i class="bx bx-arrow-back"></i>'){
+      instructions.style.display = 'none';
+     }
+     }
