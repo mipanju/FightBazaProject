82d1bd6ed720cf800d799d5628210d5fb2ed23bd
Te0o <alexandru.teodor.at@gmail.com>
2024-01-30 21:21:32 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,6 +22,57 @@ function swap(){
     }
 
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+
+
+     function resize(){
+      sidebar.classList.toggle('active');
+      if (sidebar.classList.contains('active' )){
+      SVG_male_frontElement.style.display = 'none';
+      SVG_male_backElement.style.display = 'none';
+      SVG_female_frontElement.style.display = 'none';
+      SVG_female_backElement.style.display = 'none';
+      buttonElement.style.display = 'none'
+    } else if(!sidebar.classList.contains('active') && buttonElement.innerHTML === '<i class="bx bx-male-sign"></i>' ) {
+      SVG_male_frontElement.style.display = 'inline';
+      SVG_male_backElement.style.display = 'inline';
+      SVG_female_frontElement.style.display = 'none';
+      SVG_female_backElement.style.display = 'none';
+      buttonElement.style.display = 'inline';
+    } else  {
+      SVG_male_frontElement.style.display = 'none';
+      SVG_male_backElement.style.display = 'none';
+      SVG_female_frontElement.style.display = 'inline';
+      SVG_female_backElement.style.display = 'inline';
+      buttonElement.style.display = 'inline';
+    }
+    }
+    function sidebar_toggle(){
+      sidebar.classList.toggle('active');
+    }
+     function determine(){
+      if (window.innerWidth >= 721){
+        sidebar_toggle();
+      } else{
+        resize();
+      }
+     }
+     btn.onclick = determine;
+     let instructions = document.querySelector('.exercises_display')
+     let computedStyle = window.getComputedStyle(instructions);
+     console.log(computedStyle.display);
+    
+    let buttonElement_nd = document.querySelector('.js-swap_btn2')
+    let specific_muscle = document.querySelector('.Body_map');
+    let ex_name = document.querySelector('.exercise_name');
+    let vieo_frames = document.querySelector('.video_container');
+    function Show_exercise(){
+      if (specific_muscle.id === 'chest'){
+       ex_name.innerHTML = 'Barbell Bench Press';
+=======
+>>>>>>> 63019296c6fb67afdcd872dccaf70f23a9366339
     let instructions = document.querySelector('.exercises_display')
     let buttonElement_nd = document.querySelector('.js-swap_btn2')
     let specific_muscle = document.querySelector('.Body_map');
@@ -73,6 +124,10 @@ function swap(){
        advice3.innerHTML = `<h2> <span>1</span>Lay flat on the incline bench with your feet on the ground. Raise the dumbbells until you have straight arms.</h2>
                                                <h2> <span>2</span>Lower the dumbbells to your mid chest.</h2>
                                                <h2> <span>3</span>Raise the dumbbells until you've locked your elbows.</h2>` ;
+<<<<<<< HEAD
+=======
+>>>>>>> ec439d39ed0722151fd907d527cde0bb46f1ca63
+>>>>>>> 63019296c6fb67afdcd872dccaf70f23a9366339
       }
       instructions.style.display = 'inline';
       SVG_male_frontElement.style.display = 'none';
@@ -87,9 +142,19 @@ function swap(){
       buttonElement_nd.style.display = 'none';
       buttonElement.style.display = 'inline';
     }
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+ 
+=======
+>>>>>>> 63019296c6fb67afdcd872dccaf70f23a9366339
 
     
     function sidebar_toggle(){
       sidebar.classList.toggle('active');
     }
      btn.onclick = sidebar_toggle;
+<<<<<<< HEAD
+=======
+>>>>>>> ec439d39ed0722151fd907d527cde0bb46f1ca63
+>>>>>>> 63019296c6fb67afdcd872dccaf70f23a9366339
