c8cb17c00509467737025f09e7a6dcf5f16af2b9
Te0o <alexandru.teodor.at@gmail.com>
2024-03-14 21:24:37 +0200

    d

--- js/contact.js
+++ js/contact.js
@@ -1,23 +1,11 @@
 function sendMail() {
-  var params = {
+  let params = {
     name: document.getElementById("name").value,
     email: document.getElementById("email").value,
     message: document.getElementById("message").value,
   };
 
-  const serviceID = "service_60euqwo";
-  const templateID = "template_7i256g4";
-
-    emailjs.send(serviceID, templateID, params)
-    .then(res=>{
-        document.getElementById("name").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("message").value = "";
-        console.log(res);
-        alert("Your message sent successfully!!")
-
-    })
-    .catch(err=>console.log(err));
+    emailjs.send("service_hicq2cx", "template_ysri3cw", params).then(alert("Email Sent!!"));
 
 }
 
